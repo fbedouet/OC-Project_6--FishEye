ee5e8bda86599ef9822859424a79d1c1f9680f1e
F_Bedouet <chocker@cagou.fr>
2024-01-23 15:01:28 +0100

    feat(view): Import of photographer data, integration of main page with accessibility.

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,23 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { id, name, portrait, tagline, price, city, country } = data;
     const picture = `assets/photographers/${portrait}`;
+    const url= "/photographer.html"
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const img =`
+                    <a href="/photographer.html?id=${id}" class="photographer">
+                        <div class="profil">
+                            <img src=${picture}>
+                        </div>
+                        <h2>${name}</h2>
+                    </a>
+                    <h3>${city}, ${country}</h3>
+                    <p class="tagline">${tagline}</p>
+                    <p class="price">${price}€/jour</p>
+        `
+        article.innerHTML = img;
         return (article);
     }
     return { name, picture, getUserCardDOM }
